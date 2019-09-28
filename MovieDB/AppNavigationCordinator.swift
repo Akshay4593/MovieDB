3016600fcf93c0c00349a272613d0653ed95e58c
Akshay4593 <akshayshedge4593@gmail.com>
2019-09-29 02:05:44 +0530

    Common Navigator file loaded.

---
+++ MovieDB/AppNavigationCordinator.swift
@@ -0,0 +1,18 @@
+//
+//  AppNavigationCordinator.swift
+//  MovieDB
+//
+//  Created by Akshay Shedge on 29/09/19.
+//  Copyright © 2019 Akshay Shedge. All rights reserved.
+//
+
+import Foundation
+
+class AppNavigationCordinator {
+    
+    static let shared: AppNavigationCordinator = AppNavigationCordinator()
+    
+    private init() {}
+    
+ 
+}
